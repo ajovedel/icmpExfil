76f36b0847a5f24214ddcee190b8f67036a9223c
alex <ajovedel@andrew.cmu.edu>
2016-08-11 09:43:14 -0400

    added dec_base64 func

--- icmp_exfil_server.py
+++ icmp_exfil_server.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 from optparse import OptionParser
 import socket
+import base64
 
 IPV4_HEAD_SIZE = 20
 ICMP_HEAD_SIZE = 8
+
+def dec_base64(dataBlob):
+    return base64.b64decode(dataBlob)
 
 def listen():
   s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
@@ -14,6 +18,7 @@ def listen():
     data, addr = s.recvfrom(1508)
     #print "Packet from %r: %r" % (addr,data)
 
+    if(options.encoding.lower() == "base64".lower()):
     print data[IPV4_HEAD_SIZE+ICMP_HEAD_SIZE:]
     # Skip IP and ICMP headers and starting
     # writing data at the ICMP payload field
@@ -25,6 +30,7 @@ def listen():
 if __name__ == '__main__':
 
     # Parse command line options
-
-
+    parser = OptionParser()
+    parser.add_option("-e", "--encoding", type="string", help="Encoding type (e.g. base64)")
+    (options, args) = parser.parse_args()
     listen()
